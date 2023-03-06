1db34fcf93434ee7974d1ab827f1bbb63fa87b92
SaatwikRishi <saatwik_s@mckinsey.com>
2023-03-06 16:08:29 +0530

    chore: add dockerfile

--- src/database/redis.js
+++ src/database/redis.js
@@ -4,6 +4,12 @@ const { createClient } = require('redis')
  * @description Create a redis client
  * @returns {RedisClient}
  */
-const client = createClient()
+const config = {
+  socket: {
+    host: 'redis',
+    port: 6379
+  }
+}
+const client = createClient(config)
 
 module.exports = { client }
